d6e9d15a434d6eb1468ef6e73b892a85bbf42e93
Aaron Chris Deloso <deloso.aaronchris@auf.edu.ph>
2021-09-06 14:57:13 +0800

    git hub activity 1st commit

---
+++ PDC10-Deloso/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PDC10_Deloso
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ PDC10-Deloso/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PDC10_Deloso.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        /*       public ActionResult Contact()
+               {
+                   ViewBag.Message = "Your contact page.";
+
+                   return View();
+               }      
+               public ActionResult MyProfile()
+               {
+                   ViewBag.Message = "This is My Profile Page";
+
+                   return View();
+               }
+        */
+        public ActionResult PhotoGallery()
+        {
+            ViewBag.Message = "This is My Profile Page";
+
+            return View();
+        }
+
+    }
+}
